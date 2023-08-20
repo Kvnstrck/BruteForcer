231f659a15ff7c49cd416c5a29714b04e3093d0e
Kvnstrck <strackekevin@gmail.com>
2023-08-20 17:27:18 +0200

    algorithm works for int arrays and returns the right combination

--- src/Checker.java
+++ src/Checker.java
@@ -5,7 +5,11 @@ public class Checker {
         this.password = password;
     }
 
-    public boolean check(String input){
-        return input.equals(password);
+    public boolean check(int[] input){
+        String password = "";
+        for (int digit:input) {
+            password+=digit;
+        }
+        return password.equals(this.password);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,46 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+    public static int[] input = {0, 0, 0};
+
     public static void main(String[] args) {
-        String password = "bbb";
+        String password = "111";
         Checker checker = new Checker(password);
-        //System.out.println(bruteforce(checker));
+        System.out.println(Arrays.toString(bruteforce(0, checker)));
     }
 
-    public static boolean bruteforce(Checker checker) {
+    public static int[] bruteforce(int cycleIndicator, Checker checker) {
 
-        ArrayList<Character> inputChars = new ArrayList<>();
-        inputChars.add('a');
-        //test for this length
+        if (cycleIndicator == 0) {
+            //cycle the last digit
+            while (!checker.check(input) && input[0] != 9) {
+                input[0]++;
+                System.out.println(input[0]);
+            }
+            if (checker.check(input)) return input;
+            //all combinations of last digit are false
+            for (int j : input) {
+                System.out.print(j);
+            }
 
+            input[0] = 0;
+            bruteforce(1, checker);
 
-        //set string back to just a's and add another one
-        inputChars.replaceAll(ignored -> 'a');
-        inputChars.add('a');
-        System.out.println(inputChars);
+        } else if (input[cycleIndicator] < 9) {
+            if (checker.check(input)) return input;
+            input[cycleIndicator]++;
+            bruteforce(--cycleIndicator, checker);
+        } else {//all combinations of this cycle have been false
+            if (checker.check(input)) return input;
+            //put all digits left of cycleIndicator to 0
+            cycleIndicator++;
+            for (int i = 0; i < cycleIndicator; i++) {
+                input[i] = 0;
+            }
 
+            bruteforce(cycleIndicator, checker);
+        }
 
-        return false;
+        return input;
     }
 }
